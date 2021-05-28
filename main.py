7d9da888ab53cffa528ef66b39f6bf826c8fc347
Krlstch <karolstach1999@gmail.com>
2021-05-29 01:56:12 +0200

    Added file download

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from recording_service import RecordingService
 
 import flask
 import threading
+import os
+from constants import *
 
 
 app = flask.Flask(__name__, template_folder="resources", static_folder="resources")
 
 @app.route("/")
-def index():
+def start_page():
+    return flask.render_template("start_page.html")
+
+
+@app.route("/video")
+def video():
     return flask.render_template("video.html")
 
 def gen():
@@ -19,6 +26,18 @@ def gen():
 @app.route('/stream.mjpg')
 def video_feed():
     return flask.Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route("/files")
+def files():
+    files = [filename for filename in os.listdir(DIRECTORY) if filename.endswith(".avi")]
+    return flask.render_template("files.html", len=len(files), files=files)
+
+@app.route("/files/download/<path:path>")
+def get_file(path):
+    if os.path.isfile(os.path.join(DIRECTORY, path)):
+        return flask.send_from_directory(DIRECTORY, path, as_attachment=True)
+    else:
+        return flask.render_template("error.html", error_message="File not found")
 
 
 if __name__ == "__main__":
