daf425eb964e40ebbb158b3270ddfd7f3b855ca9
Krlstch <karolstach1999@gmail.com>
2021-05-29 02:08:34 +0200

    replaced queue with more logical system

--- streaming_output.py
+++ streaming_output.py
@@ -3,13 +3,19 @@ import queue
 
 class StreamingOutput:
     def __init__(self):
-        self.queue = queue.Queue(maxsize=1)
+        self.frame = None
+        self.new_frame_present = False
+        self.condition = threading.Condition()
 
     def set_frame(self, frame):
-        if self.queue.full():
-            self.queue.get()
-        self.queue.put(frame)
+        with self.condition:
+            self.frame = frame
+            self.new_frame_present = True
+            self.condition.notify()
     
     def get_frame(self):
-        return self.queue.get(block=True)
+        with self.condition:
+            if not self.new_frame_present:
+                self.condition.wait()
+            return self.frame
             
